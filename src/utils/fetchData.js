a6e9a977f729c38fa3804da1909ba380a208ff9a
PareshNair <pareshnair2411@gmail.com>
2023-09-14 13:18:08 +0530

    ..

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,10 +1,10 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-        //'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Key': import.meta.env.VITE_SOME_KEY,
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
 
-        'X-RapidAPI-Key': 'c84994802emsha30802dea6cf0a2p1a1d90jsnc084dbac45cb'
+        //'X-RapidAPI-Key': 'c84994802emsha30802dea6cf0a2p1a1d90jsnc084dbac45cb'
     }
   };
   
