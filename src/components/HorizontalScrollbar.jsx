8460b7e32971870a1f7641493646a033748cff31
PareshNair <pareshnair2411@gmail.com>
2023-09-14 23:00:07 +0530

    ..

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -1,18 +1,17 @@
-import { useContext } from 'react';
-import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
-import { Box, Typography } from '@mui/material';
-
-// import ExerciseCard from './ExerciseCard';
-import BodyPart from './BodyPart';
-import RightArrowIcon from '../assets/icons/right-arrow.png';
-import LeftArrowIcon from '../assets/icons/left-arrow.png';
+import { useContext } from "react";
+import { Box, Typography, Stack } from "@mui/material";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import RightArrowIcon from "../assets/icons/right-arrow.png";
+import LeftArrowIcon from "../assets/icons/left-arrow.png";
+import Icon from "../assets/icons/gym.png";
+import ExerciseCard from "./ExerciseCard";
 
 const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
 
   return (
     <Typography onClick={() => scrollPrev()} className="right-arrow">
-      <img src={LeftArrowIcon} alt="right-arrow" />
+      <img src={LeftArrowIcon} alt="left-arrow" />
     </Typography>
   );
 };
@@ -27,21 +26,60 @@ const RightArrow = () => {
   );
 };
 
-const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
-  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-    {data.map((item) => (
-      <Box
-        key={item.id || item}
-        itemID={item.id || item}
-        title={item.id || item}
-        m="0 40px"
-      >
-        {/* {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> } */}
-        <BodyPart item={item} bodyPart = 
-             {bodyPart} setBodyPart = {setBodyPart} />
-      </Box>
-    ))}
-  </ScrollMenu>
-);
+const HorizontalScrollbar = ({ data, bodyPart, setBodyPart, isBodyPart }) => {
+  return (
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+      {data.map((item) => (
+        <Box
+          key={item.id || item}
+          itemId={item.id || item}
+          title={item.id || item}
+          m="0 40px"
+        >
+          {isBodyPart ? (
+            <Stack
+              type="button"
+              alignItems="center"
+              justifyContent="center"
+              className="bodyPart-card"
+              sx={{
+                borderTop: bodyPart === item ? "4px solid #FF2625" : "",
+                backgroundColor: "#FFFFFF",
+                borderBottomLeftRadius: "20px",
+                width: "270px",
+                height: "280px",
+                gap: "47px",
+                cursor: "pointer",
+              }}
+              onClick={() => {
+                setBodyPart(item);
+                window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+              }}
+            >
+              <img
+                src={Icon}
+                alt="dumbbell"
+                style={{
+                  width: "40px",
+                  height: "40px",
+                }}
+              />
+              <Typography
+                fontSize="24px"
+                fontWeight="bold"
+                color="#3A1212"
+                textTransform="capitalize"
+              >
+                {item}
+              </Typography>
+            </Stack>
+          ) : (
+            <ExerciseCard exercise={item} />
+          )}
+        </Box>
+      ))}
+    </ScrollMenu>
+  );
+};
 
 export default HorizontalScrollbar;
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,10 +1,18 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': import.meta.env.VITE_SOME_KEY,
+        'X-RapidAPI-Key': import.meta.env.VITE_ANOTHER_RAPID_API_KEY,
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
 
         //'X-RapidAPI-Key': 'c84994802emsha30802dea6cf0a2p1a1d90jsnc084dbac45cb'
+    }
+  };
+  
+ export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+        'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
     }
   };
   
